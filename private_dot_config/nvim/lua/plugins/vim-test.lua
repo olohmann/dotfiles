031de41b439fce1a28c237131b1b22acfcadf6b5
Oliver Lohmann <oliver@lohmann.io>
2024-03-28 22:31:58 +0100

    tmux tuning.

---
+++ private_dot_config/nvim/lua/plugins/vim-test.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "vim-test/vim-test",
+    dependencies = { "preservim/vimux" },
+    cmd = {
+      "TestNearest",
+      "TestFile",
+      "TestSuite",
+      "TestLast",
+      "TestVisit",
+    },
+    keys = {
+      { "<leader>t", "<cmd>TestNearest<cr>" },
+      { "<leader>T", "<cmd>TestFile<cr>" },
+      { "<leader>a", "<cmd>TestSuite<cr>" },
+      { "<leader>l", "<cmd>TestLast<cr>" },
+      { "<leader>g", "<cmd>TestVisit<cr>" },
+    },
+    config = function()
+      vim.cmd("let test#strategy = 'vimux'")
+    end,
+  },
+}
