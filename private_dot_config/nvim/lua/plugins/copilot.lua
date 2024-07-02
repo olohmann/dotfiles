279363b9e94b78507b37d646aaf66d237094dc96
Oliver Lohmann <oliver@lohmann.io>
2024-07-02 17:27:03 +0200

    Config updates.

---
+++ private_dot_config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      filetypes = {
+        yaml = false,
+        markdown = false,
+        help = false,
+        gitcommit = false,
+        gitrebase = false,
+        hgcommit = false,
+        svn = false,
+        cvs = false,
+        ["."] = false,
+        org = false,
+        norg = false,
+      },
+    },
+  },
+}
---
+++ private_dot_config/nvim/lua/plugins/vim-bicep.lua
@@ -0,0 +1,5 @@
+return {
+  {
+    "carlsmedstad/vim-bicep",
+  },
+}
